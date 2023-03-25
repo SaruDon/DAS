2fc0f9161fcb90aed666a30eaedebe5390b7301b
Sarvesh Khamkar <93531912+SaruDon@users.noreply.github.com>
2023-03-25 12:10:31 +0530

    Add files via upload

---
+++ 2D_Array/diagonalSum.java
@@ -0,0 +1,35 @@
+public class diagonalSum {
+
+public static int diagonalSum(int matrix[][]) {
+    int sum =0;
+    
+   /* for(int i=0;i<=matrix.length-1;i++){                //t(n)=n*n
+        for(int j=0;j<=matrix[0].length-1;j++){
+            if( i==j){
+                sum += matrix[i][j];
+            }
+            else if( i+j == matrix.length-1){
+                sum += matrix[i][j];
+            }
+        }
+    }
+    */
+
+    for(int i=0;i<=matrix.length-1;i++){
+
+        sum += matrix[i][i];
+
+        sum += matrix [i][matrix.length-1-i];
+    }
+    
+    return sum;
+}
+
+    public static void main(String[] args) {
+        int matrix[][] ={{1,2,3,4},
+                        {5,6,7,8},
+                     {9,10,11,12},
+                    {13,14,15,16}};
+        System.out.println(diagonalSum(matrix));
+    }
+}
---
+++ 2D_Array/hw/tranpose.java
@@ -0,0 +1,13 @@
+
+
+public class tranpose {
+
+    public static void transpose(int arr[][]) {
+        int row = arr.length-1;
+        int col = arr[0].length-1;
+    }
+    public static void main(String[] args) {
+        int[][] arr = { {1,4,9},
+                        {11,4,3},};
+    }    
+}
