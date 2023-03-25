5f7b76f325310b072c0710ea99305cfd482d2e1f
Sarvesh Khamkar <93531912+SaruDon@users.noreply.github.com>
2023-03-25 12:13:46 +0530

    Add files via upload

---
+++ sort/bubble_sort.java
@@ -0,0 +1,27 @@
+
+public class bubble_sort {
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // SWAP
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 5, 4, 1, 2, 3 };
+        bubbleSort(nums);
+    }
+
+}
---
+++ sort/selection_sort.java
@@ -0,0 +1,29 @@
+
+public class selection_sort {
+
+    public static void selection(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 5, 4, 1, 2, 3, 6 };
+        selection(nums);
+    }
+}
